b12aaa2f205502d3246d1e48ad54934ca7220c3a
hudo-ng <hmdong901026@gmail.com>
2025-01-17 14:32:40 -0700

    add prettier config

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,10 +1,10 @@
 import StudentInfo from "./student-info";
 
 export default function Page() {
-    return (
-      <main>
-        <h2>Shopping List</h2>
-        <StudentInfo />
-      </main>
-    );
-  }
+  return (
+    <main>
+      <h2>Shopping List</h2>
+      <StudentInfo />
+    </main>
+  );
+}
